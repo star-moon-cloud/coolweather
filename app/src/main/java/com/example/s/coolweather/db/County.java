bec15b4f3888f3e7f4d7217b78e0e0203494189f
wang <582527387@qq.com>
2017-08-12 20:58:45 +0800

    put db and list

---
+++ app/src/main/java/com/example/s/coolweather/db/County.java
@@ -0,0 +1,38 @@
+package com.example.s.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by s on 2017/8/12.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getcountyName(){
+        return countyName;
+    }
+    public void setcountyName(String countyName){
+        this.countyName = countyName;
+    }
+    public String getWeatherId(){
+        return weatherId;
+    }
+    public void setWeatherId(String weatherId){
+        this.weatherId = weatherId;
+    }
+    public int getCityId(){
+        return cityId;
+    }
+    public void setCityId(int cityId){
+        this.cityId=cityId;
+    }
+}
